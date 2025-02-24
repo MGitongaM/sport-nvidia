361664e33de518dcb49d24fc9752c5b501935ed5
Michael Gitonga <gitoshmbae@gmail.com>
2025-02-24 09:01:06 +0300

    Adds the sheet component from shadcnui, Adds the mobile navigation section in src/components/NavigationSection.tsx

--- src/components/NavigationSection.tsx
+++ src/components/NavigationSection.tsx
@@ -5,7 +5,17 @@ import {
   NavigationMenuList,
 } from "./ui/navigation-menu";
 import Image from "next/image";
-import logo from "../../public/images/transparent-logo.png"
+import logo from "../../public/images/transparent-logo.png";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { MenuIcon } from "lucide-react";
 export const navLinks = [
   {
     id: 1,
@@ -30,33 +40,82 @@ export const navLinks = [
 ];
 export default function NavigationSection() {
   return (
-    <NavigationMenu className="bg-green-50 overflow-hidden">
-      <NavigationMenuList>
-        <div className="flex flex-wrap md:flex-nowrap w-screen justify-between items-center px-16">
-          <NavigationMenuItem>
-            <Link href="/">
-              <Image
-                src={logo}
-                height={200}
-                width={200}
-                alt="logo"
-                className="object-contain w-24"
-              />
-            </Link>
-          </NavigationMenuItem>
-          <div className="flex flex-wrap md:flex-nowrap gap-5 items-center justify-end w-full">
-            {navLinks.map((link) => (
-              <NavigationMenuItem key={link.id}>
-                <ul>
-                  <Link href={link.hrefLink}>
-                    <li className="hover:text-green-500 font-semibold text-sm">{link.text}</li>
-                  </Link>
-                </ul>
+    <>
+      {/* Desktop Navigation  */}
+      <div className="hidden md:block">
+        <NavigationMenu className="bg-green-50 overflow-hidden">
+          <NavigationMenuList>
+            <div className="flex flex-wrap md:flex-nowrap w-screen justify-between items-center px-16">
+              <NavigationMenuItem>
+                <Link href="/">
+                  <Image
+                    src={logo}
+                    height={200}
+                    width={200}
+                    alt="logo"
+                    className="object-cover w-28"
+                  />
+                </Link>
               </NavigationMenuItem>
-            ))}
-          </div>
-        </div>
-      </NavigationMenuList>
-    </NavigationMenu>
+              <div className="flex flex-wrap md:flex-nowrap gap-5 items-center justify-end w-full">
+                {navLinks.map((link) => (
+                  <NavigationMenuItem key={link.id}>
+                    <ul>
+                      <Link href={link.hrefLink}>
+                        <li className="hover:text-green-500 font-semibold text-sm">
+                          {link.text}
+                        </li>
+                      </Link>
+                    </ul>
+                  </NavigationMenuItem>
+                ))}
+              </div>
+            </div>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      {/* Mobile Navigation */}
+      <div className="flex justify-between px-4 bg-green-50 md:hidden">
+        <Link href="/">
+          <Image
+            src={logo}
+            height={200}
+            width={200}
+            alt="logo"
+            className="object-cover w-28"
+          />
+        </Link>
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon />
+          </SheetTrigger>
+          <SheetContent className="bg-green-50">
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+            <NavigationMenu className="">
+              <NavigationMenuList>
+                <div className="flex flex-col mt-10 gap-5 items-start justify-end w-full">
+                  {navLinks.map((link) => (
+                    <NavigationMenuItem key={link.id}>
+                      <ul>
+                        <Link href={link.hrefLink}>
+                          <SheetClose asChild>
+                            <li className="hover:text-green-500 font-semibold text-sm ">
+                              {link.text}
+                            </li>
+                          </SheetClose>
+                        </Link>
+                      </ul>
+                    </NavigationMenuItem>
+                  ))}
+                </div>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </>
   );
 }
