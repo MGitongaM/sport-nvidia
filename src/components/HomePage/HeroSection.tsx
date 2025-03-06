4ca8086819bf694b6be2145817f6ce2a8c8a5337
Michael Gitonga <gitoshmbae@gmail.com>
2025-03-06 11:57:51 +0300

    Üpdates the home page hero section in terms of the copy and image

--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -10,39 +10,36 @@ export default async function HeroSection() {
   });
   return (
     <>
-      <section className="min-h-[80vh] ">
-        <div className="relative flex flex-col min-h-[80vh]  items-start justify-center px-4 ">
-          <div className="container mx-auto -mt-28">
-            <div className="max-w-lg space-y-6 bg-white/0 backdrop-blur-lg px-4 py-10 rounded-lg">
-              <h1 className="text-3xl md:text-5xl font-bold text-blue-300">
-                {banner?.[0].bannerTitle}
-              </h1>
-              <p className="text-xl font-semibold text-slate-300">
-                {/* Nakuru Athletic Club (N.A.C.) is a members-only sports , social
-                club conveniently with recreational facilities. */}
-                Nakuru Athletic Club (N.A.C.) is  where sports enthusiasts
-                and families come together to enjoy a wide range of activities.
-              </p>
-              <Button className="bg-white hover:bg-blue-200 text-black">
-                Join Us Today
-              </Button>
-            </div>
+      <section className=" ">
+        <div className="flex flex-wrap md:flex-nowrap  min-h-[80vh] lg:h-[80vh]">
+          <div className="w-full md:w-[36rem] flex flex-col justify-center gap-8 h-auto bg-black pl-10 lg:pl-20 pr-4 py-8 text-slate-300">
+            <h1 className="text-3xl md:text-5xl font-bold">
+              {banner?.[0].bannerTitle}
+            </h1>
+            <p className="text-xl font-semibold">
+              {banner?.[0].bannerText}
+            </p>
+            <Button className="bg-white w-44 hover:bg-blue-200 text-black font-semibold">
+              Join Us Today
+            </Button>
+          </div>
+          <div className=" grow bg-black">
+            {banner?.[0].bannerImage ? (
+              <Image
+                src={urlFor(banner?.[0].bannerImage)
+                  .width(1200)
+                  .height(900)
+                  .quality(80)
+                  .auto("format")
+                  .url()}
+                height={900}
+                width={800}
+                alt={banner?.[0].bannerText || ""}
+                className="object-cover object-top h-full  w-full "
+              />
+            ) : null}
           </div>
         </div>
-        {banner?.[0].bannerImage ? (
-          <Image
-            src={urlFor(banner?.[0].bannerImage)
-              .width(1200)
-              .height(900)
-              .quality(80)
-              .auto("format")
-              .url()}
-            height={900}
-            width={800}
-            alt={banner?.[0].bannerText || ""}
-            className="object-cover h-full w-full absolute top-16 -z-10"
-          />
-        ) : null}
       </section>
     </>
   );
