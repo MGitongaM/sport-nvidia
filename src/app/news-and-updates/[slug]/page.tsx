5cbbe9581c39c6de064f5226ad9dbd6a9a7b7ba7
Michael Gitonga <gitoshmbae@gmail.com>
2025-03-10 15:56:11 +0300

    Updates the UI of the src/app/news-and-updates/[slug]/page.tsx

--- src/app/news-and-updates/[slug]/page.tsx
+++ src/app/news-and-updates/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function page({
   });
   return (
     <>
-      <section className="bg-zinc-800">
+      <section className="bg-zinc-800 pt-32">
         <div className="max-w-5xl mx-auto px-4">
           <div className="pt-4">
             {data?.newsUpdateFeatureImage ? (
@@ -41,18 +41,19 @@ export default async function page({
             <h1 className="text-3xl md:text-5xl font-bold text-blue-400">
               {data?.newsUpdateTitle}
             </h1>
-            <div className="flex gap-4 text-slate-300">
-              <p className="c">{data?.newsUpdateDate}</p>
-              <p className="c">
-                <span className="font-semibold">Author:</span>{" "}
+            <div className="flex justify-between gap-4 text-slate-300">
+              
+              <p className="bg-zinc-700 px-4 py-2 rounded-lg text-sm">
+                <span className="font-semibold">Author:</span>
                 {data?.newsUpdateAuthor}
               </p>
+              <p className="bg-zinc-700 px-4 py-2 rounded-lg text-sm">{data?.newsUpdateDate}</p>
             </div>
             <hr />
             <div className="space-y-6">
               {data?.newsUpdateDetails != null &&
               data?.newsUpdateDetails != undefined ? (
-                <div className="prose  max-w-5xl prose-p:text-slate-300 prose-h1:text-slate-200 prose-h2:text-slate-200 prose-h3:text-slate-200 prose-h4:text-slate-200 prose-h5:text-slate-200 prose-h6:text-slate-200 prose-text-slate-300 prose-strong:text-slate-300 prose-ul:text-slate-300">
+                <div className="prose prose:lg  max-w-5xl prose-p:text-slate-300 prose-h1:text-slate-200 prose-h2:text-slate-200 prose-h3:text-slate-200 prose-h4:text-slate-200 prose-h5:text-slate-200 prose-h6:text-slate-200 prose-text-slate-300 prose-strong:text-slate-300 prose-ul:text-slate-300">
                   <PortableText
                     value={data?.newsUpdateDetails}
                     components={components}
